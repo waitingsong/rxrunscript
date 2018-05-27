66fc977be39d4fcd1e10970056eb9cc64559a3aa
waiting <1661926154@qq.com>
2018-05-27 19:37:41 +0800

    test: index

---
+++ test/20_index.test.ts
@@ -0,0 +1,104 @@
+/// <reference types="mocha" />
+
+import * as assert from 'power-assert'
+import rewire = require('rewire')
+import * as rmdir from 'rimraf'
+import { of } from 'rxjs'
+import { buffer, reduce, tap } from 'rxjs/operators'
+
+import runScript from '../src/index'
+import {
+  basename,
+  join,
+} from '../src/shared/index'
+
+
+
+const filename = basename(__filename)
+
+
+describe(filename, () => {
+
+  it('Should works running openssl', done => {
+    runScript('openssl version')
+      .subscribe(
+        buf => {
+          try {
+            const ret = buf.toString()
+            assert(ret.includes('OpenSSL'), `result: "${ret}"`)
+          }
+          catch (ex) {
+            assert(false, ex)
+          }
+        },
+        err => {
+          assert(false, err)
+          done()
+        },
+        done,
+    )
+  })
+
+  it('Should works running openssl with invalid args', done => {
+    runScript('openssl fake')
+      .subscribe(
+        buf => {
+          try {
+            const ret = buf.toString()
+            assert(!ret.includes('OpenSSL'), `should got error but got result: "${ret}"`)
+          }
+          catch (ex) {
+            assert(true)
+          }
+        },
+        err => {
+          assert(true)
+          done()
+        },
+        done,
+    )
+  })
+
+
+})
+
+
+describe(filename, () => {
+  const file = join(__dirname, 'interval-source.ts')
+
+  it('Should works running interval-source.ts with random count', done => {
+    let count = Math.floor(Math.random() * 10)
+    const options = {
+      cwd: __dirname, // ! for test/tsconfig.json
+    }
+
+    if (count === 0) {
+      count = 5
+    }
+
+    runScript(`ts-node ${file} ${count}`, options).pipe(
+        tap(buf => console.log('got:', buf.toString().trim())),
+        reduce((acc: Buffer[], curr: Buffer) => {
+          acc.push(curr)
+          return acc
+        }, []),
+    )
+      .subscribe(
+        arr => {
+          try {
+            assert(arr.length === count, `should got array with ${count} items but got: "${arr}"`)
+          }
+          catch (ex) {
+            assert(false, ex)
+          }
+        },
+        err => {
+          assert(false, err)
+          done()
+        },
+        done,
+    )
+  })
+
+
+})
---
+++ test/interval-source.ts
@@ -0,0 +1,16 @@
+import { interval } from 'rxjs'
+import * as yargs from 'yargs'
+
+import { take } from '../node_modules/rxjs/operators'
+
+const args = yargs.argv._
+const limit = +args[0]
+
+if (Number.isNaN(limit)) {
+  throw new Error('take number invaid')
+}
+
+interval(1000).pipe(
+  take(limit),
+)
+  .subscribe(console.info)
