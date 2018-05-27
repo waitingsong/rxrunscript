533f0ba638a4b9694ee86057d925b94ba9bbd3cf
waiting <1661926154@qq.com>
2018-05-27 17:47:03 +0800

    feat: runScript

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { spawn, SpawnOptions } from 'child_process'
+import { of, throwError, Observable, Observer } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+
+import { join } from './shared/index'
+
+
+function runScript(script: string, options?: SpawnOptions, maxErrorBufferCount?: number): Observable<Buffer> {
+  const initialOpts: SpawnOptions = {
+    cwd: process.cwd(),
+    env: { ...process.env },
+    stdio: 'pipe',
+  }
+  const opts = options ? { ...initialOpts, ...options } : initialOpts
+  let sh = 'sh'
+  let shFlag = '-c'
+
+  script = script.replace(/\\/g, '/').trimLeft()
+
+  if (process.platform === 'win32') {
+    sh = process.env.comspec || 'cmd'
+    shFlag = '/d /s /c'
+    opts.windowsVerbatimArguments = true
+
+    if (script.slice(0, 2) === './' || script.slice(0, 3) === '../') {
+      const arr = script.split(' ')
+
+      arr[0] = join(<string> opts.cwd, arr[0])
+      script = arr.join(' ')
+    }
+  }
+
+  const run$: Observable<Buffer> = Observable.create((obv: Observer<Buffer | string>) => {
+    const proc = spawn(sh, [shFlag, script], opts)
+    const stderr = <Buffer[]> []
+    const errBufLimit = typeof maxErrorBufferCount === 'number' && maxErrorBufferCount > 0
+      ? maxErrorBufferCount
+      : 0
+
+    if (proc.stdout) {
+      proc.stdout.on('data', buf => obv.next(typeof buf === 'string' ? Buffer.from(buf) : buf))
+    }
+    if (proc.stderr) {
+      proc.stderr.on('data', buf => {
+        if (stderr.length <= errBufLimit) {
+          stderr.push(typeof buf === 'string' ? Buffer.from(buf) : buf)
+        }
+        else {
+          const errMsg = Buffer.concat(stderr).toString()
+
+          obv.error(new Error(`Run script with error: "${sh} ${shFlag} ${script}"\n${errMsg}`))
+          proc.kill()
+        }
+      })
+    }
+
+    proc.on('error', err => obv.error(err))
+
+    proc.on('close', code => {
+      let errMsg = ''
+
+      if (stderr.length > 0) {
+        errMsg = Buffer.concat(stderr).toString()
+      }
+
+      if (code !== 0 || errMsg) {
+        obv.error(new Error(`Run script with error code ${code}: "${sh} ${shFlag} ${script}"\n${errMsg}`))
+      }
+      else {
+        obv.complete()
+      }
+    })
+
+    return () => proc.kill()  // for unsubscribe()
+  })
+
+  return run$
+}
+
+export default runScript
