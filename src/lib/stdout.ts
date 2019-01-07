5e68df8f3d6e102a3dbf5c0692c7e1d2d9fc28c8
waiting <1661926154@qq.com>
2019-01-07 17:27:35 +0800

    chore: prune

--- src/lib/stdout.ts
+++ src/lib/stdout.ts
@@ -1,10 +1,8 @@
 import { fromEvent, Observable } from 'rxjs'
+// import { tap } from 'rxjs/operators'
 
 
 export function bindStdoutData(stdout: NodeJS.ReadableStream): Observable<Buffer> {
-  stdout.on('data', data => {
-
-  })
   const data$ = fromEvent<Buffer>(stdout, 'data').pipe(
     // tap(buf => console.info('stdout:', buf.toString())),
   )
