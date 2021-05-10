c9db365ace1ad8ab40f6d14b8297994b2185cff9
waiting <1661926154@qq.com>
2021-05-10 15:21:58 +0800

    test: 12

---
+++ test/21_index.test.ts
@@ -0,0 +1,96 @@
+import { error } from 'console'
+import { sep } from 'path'
+
+import {
+  basename,
+  join,
+} from '@waiting/shared-core'
+import { concat, from as ofrom, of, EMPTY } from 'rxjs'
+import {
+  catchError,
+  concatMap,
+  defaultIfEmpty,
+  filter,
+  finalize,
+  mergeMap,
+  tap,
+  timeout,
+} from 'rxjs/operators'
+
+import { run, RxRunFnArgs } from '../src/index'
+
+import { opensslCmds, testIntervalSource } from './helper'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = basename(__filename)
+
+
+describe(filename, () => {
+  const file = join(__dirname, 'interval-source.ts')
+  const tsConfig = join(__dirname, 'tsconfig.json')
+  let count = Math.floor(Math.random() * 10)
+  const options = {
+    cwd: __dirname, // ! for test/tsconfig.json
+  }
+
+  if (count < 3) {
+    count = 3
+  }
+  const cmds: RxRunFnArgs[] = [
+    [`ts-node -P "${tsConfig}" ${file} ${count}`, null, options],
+    [' ts-node ', [` -P "${tsConfig}" ${file} ${count}`], options],
+    ['ts-node ', [`-P "${tsConfig}"`, file, count.toString()], options],
+  ]
+
+  it('Should works running interval-source.ts with random count serially', (done) => {
+    if (process.platform === 'win32') {
+      console.info('skip test under win32')
+      return done()
+    }
+    console.info('start test count serially:', count)
+
+    ofrom(cmds).pipe(
+      concatMap(([cmd, args, options2]) => {
+        const opts = { ...options2 }
+        if (process.platform === 'win32') {
+          opts.cwd = 'c:/Program Files/Git/mingw64/bin'
+        }
+        return testIntervalSource(cmd, args, opts, count)
+      }),
+      finalize(() => done()),
+      catchError((err: Error) => {
+        assert(false, err.message)
+        return EMPTY
+      }),
+    ).subscribe()
+  })
+
+  it('Should works running interval-source.ts with random count parallelly', (done) => {
+    if (process.platform === 'win32') {
+      console.info('skip test under win32')
+      return done()
+    }
+    console.info('start test count parallelly:', count)
+
+    ofrom(cmds).pipe(
+      // mergeMap(([cmd, args, opts]) => testIntervalSource(cmd, args, opts, count)),
+      mergeMap(([cmd, args, options2]) => {
+        const opts = { ...options2 }
+        if (process.platform === 'win32') {
+          opts.cwd = 'c:/Program Files/Git/mingw64/bin'
+        }
+        return testIntervalSource(cmd, args, opts, count)
+      }),
+      finalize(() => done()),
+      catchError((err: Error) => {
+        assert(false, err.message)
+        return EMPTY
+      }),
+    ).subscribe()
+  })
+
+})
+
---
+++ test/23_index.test.ts
@@ -0,0 +1,168 @@
+import { error } from 'console'
+import { sep } from 'path'
+
+import {
+  basename,
+  join,
+} from '@waiting/shared-core'
+import { concat, from as ofrom, of, EMPTY } from 'rxjs'
+import {
+  catchError,
+  concatMap,
+  defaultIfEmpty,
+  filter,
+  finalize,
+  mergeMap,
+  tap,
+  timeout,
+} from 'rxjs/operators'
+
+import { run, RxRunFnArgs } from '../src/index'
+
+import { opensslCmds, testIntervalSource } from './helper'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = basename(__filename)
+
+
+
+describe(filename, () => {
+  const file = 'openssl.sh'
+  const path = join(__dirname, file)
+  const appDirName = __dirname.split(sep).slice(-2, -1)[0]
+
+  if (process.platform === 'win32') {
+    console.info('skip test under win32')
+    return
+  }
+  console.info('Current path:', __dirname)
+  console.info('process.cwd:', process.cwd())
+
+  it(`Should running "${file}" works without Permission`, (done) => {
+    assert(typeof appDirName === 'string' && appDirName.length > 0, 'Working folder invalid')
+
+    // must inner it()
+    const chmod$ = run('chmod a-x', [join(__dirname, file)])
+    const ls$ = run('ls -al', [path]).pipe(
+      tap(buf => console.log('file should has no x rights:\n', buf.toString())),
+    )
+
+    const cmds: RxRunFnArgs[] = [
+      [path],
+      [`./test/${file}`],
+      [`../${appDirName}/test/${file}`],
+    ]
+    const ret$ = ofrom(cmds).pipe(
+      concatMap(([cmd, args, opts]) => {
+        return run(cmd, args, opts).pipe(
+          tap((buf) => {
+            const ret = buf.toString().trim()
+            assert(! ret.includes('OpenSSL '), `Should not output OpenSSL version. But result: "${ret}"`)
+          }),
+          catchError((err: Error) => {
+            const msg = err.message
+            assert(
+              msg.includes('Permission denied') && msg.includes(file),
+              'Should throw error cause of Permission denied',
+            )
+            return of(Buffer.from(''))
+          }),
+        )
+      }),
+      finalize(() => done()),
+    )
+
+    concat(chmod$, ls$, ret$)
+      .pipe(timeout(30000))
+      .subscribe()
+  })
+
+  it(`Should running "${file}" works`, (done) => {
+    assert(typeof appDirName === 'string' && appDirName.length > 0, 'Working folder invalid')
+
+    const isTravis = __dirname.includes('travis')
+
+    // must inner it()
+    const chmod$ = run('chmod u+x', [join(__dirname, file)])
+    const ls$ = run('ls -al', [path]).pipe(
+      tap(buf => console.log('file should has x rights:\n', buf.toString())),
+    )
+    const cat$ = run('cat', [path]).pipe(
+      tap(buf => console.log('cat file result:\n', buf.toString())),
+    )
+
+    const cmds: RxRunFnArgs[] = [
+      ['sh', [path] ],
+      [path],
+      [`./test/${file}`],
+      [`../${appDirName}/test/${file}`],
+    ]
+    const ret$ = ofrom(cmds).pipe(
+      filter(([cmd, args], index) => {
+        console.info(`\nStarting cmds: "${cmd}"`, args && args.length ? args[0] : '')
+        const skipped = ! isTravis
+        if (isTravis) {
+          console.info(
+            `Skip "${cmd}" under travis cause of file will not found. But test passed under local test!`,
+          )
+        }
+        return skipped
+      }),
+      mergeMap(([cmd, args, opts]) => {
+        return run(cmd, args, opts).pipe(
+          defaultIfEmpty(Buffer.from('foo')),
+          tap((buf) => {
+            const ret = buf.toString().trim()
+            console.info('Runner script result:' + ret)
+            console.info('Runner script result buf:', buf)
+            assert(ret.includes('OpenSSL '), `Should output OpenSSL version. But result: "${ret}"`)
+          }),
+          timeout(5000),
+        )
+      }),
+      finalize(() => done()),
+    )
+
+    concat(chmod$, ls$, cat$, ret$)
+      .pipe(timeout(50000))
+      .subscribe()
+  })
+})
+
+
+describe(filename, () => {
+  it('Should works without any output', (done) => {
+    const cmds: RxRunFnArgs[] = [
+      ['cd /'],
+      ['cd ..'],
+      ['cd', ['/'] ],
+      ['cd', ['..'] ],
+    ]
+
+    ofrom(cmds).pipe(
+      mergeMap(([cmd, args, opts]) => {
+        return run(cmd, args, opts).pipe(
+          defaultIfEmpty(Buffer.from('Should empty value')),
+          timeout(5000),
+        )
+      }),
+    )
+      .subscribe({
+        next: (buf: Buffer) => {
+          assert(! buf.byteLength, 'Should result empty, but got: ' + buf.toString())
+        },
+        error: (err) => {
+          assert(false, err)
+          done()
+        },
+        complete: () => {
+          done()
+        },
+      })
+
+  })
+})
+
