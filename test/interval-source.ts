757dfa4a427781eceef8c6ef41cf933819654d2e
waiting <1661926154@qq.com>
2021-06-18 11:32:16 +0800

    test: fix

--- test/interval-source.ts
+++ test/interval-source.ts
@@ -1,17 +1,21 @@
+// @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { interval } from 'rxjs'
 import { take } from 'rxjs/operators'
 // eslint-disable-next-line node/no-unpublished-import
 import yargs from 'yargs'
 
 
-const args = yargs.argv._ as unknown
-const limit = +args[0]
+const args: (string|number)[] = typeof yargs.argv._ === 'object'
+  ? yargs.argv._
+  : []
+const limit = args[0] ? +args[0] : 'fake'
 
 if (Number.isNaN(limit)) {
   throw new Error('take number invaid')
 }
 
 interval(1000).pipe(
-  take(limit),
+  take(limit as number),
 )
   .subscribe(console.info)
