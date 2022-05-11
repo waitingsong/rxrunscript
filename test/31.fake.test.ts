7ddb42673ed216f7ec71ff2929256d4bd13bcdac
waiting <1661926154@qq.com>
2022-05-11 11:06:04 +0800

    test: add test/31.fake.test.ts

---
+++ test/31.fake.test.ts
@@ -0,0 +1,45 @@
+import assert from 'assert/strict'
+import { relative } from 'path'
+
+import { from as ofrom } from 'rxjs'
+import { mergeMap } from 'rxjs/operators'
+
+import { run, RxRunFnArgs } from '../src/index'
+
+
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
+
+describe(filename, () => {
+  it('Should work running fake cmd', (done) => {
+    const cmds: RxRunFnArgs[] = [ ['fakexxx '] ]
+
+    ofrom(cmds).pipe(
+      mergeMap(([cmd, args, options]) => {
+        const opts = { ...options }
+        if (process.platform === 'win32') {
+          opts.cwd = 'c:/Program Files/Git/mingw64/bin'
+        }
+        return run(cmd, args, opts)
+      }, 1), // parallel may cause empty result!
+    )
+      .subscribe({
+        next: (val) => {
+          if (Buffer.isBuffer(val)) {
+            throw new TypeError('Should not got stdout data:' + val.toString())
+          }
+          else if (val.exitCode !== 0) {
+            throw new TypeError('Should get exitCode zero but got data:' + val.exitCode.toString())
+          }
+        },
+        error: (ex: Error) => {
+          assert(ex.message.includes('fakexxx'), ex.message)
+          done()
+        },
+        complete: () => done(),
+      })
+
+  })
+
+
+})
+
